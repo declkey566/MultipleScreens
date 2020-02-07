92d0764fb22ab91b549948e5e3146c3f7f304ea6
Tony Theodoropoulos <theodot@outlook.com>
2020-02-07 11:04:19 -0500

    Add project files.

---
+++ MultipleScreens/FirstScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleScreens
+{
+    public partial class FirstScreen : UserControl
+    {
+        public FirstScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1.inputString = textBox1.Text;
+
+
+            Form f = this.FindForm();
+            f.Controls.Remove(this);
+
+            SecondScreen ss = new SecondScreen();
+            f.Controls.Add(ss);
+        }
+    }
+}
---
+++ MultipleScreens/SecondScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleScreens
+{
+    public partial class SecondScreen : UserControl
+    {
+        public SecondScreen()
+        {
+            InitializeComponent();
+            label1.Text = Form1.inputString;
+        }
+    }
+}
